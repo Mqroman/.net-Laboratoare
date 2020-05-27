46a7fbfe93619da147e9a296fba4fd1034a2e0e9
Mqroman <48622213+Mqroman@users.noreply.github.com>
2020-05-27 22:44:17 +0300

    Add files via upload

---
+++ proiectul 3/ObjectWCF/IMedia.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.ServiceModel;
+
+namespace ObjectWCF
+{
+    [ServiceContract]
+    interface IMedia
+    {
+        [OperationContract]
+        void SaveMedia(string path, string events, string persons, string peisaj, string locatie, string altele, DateTime creationDate);
+        [OperationContract]
+        void DeleteMedia(string path);
+        [OperationContract]
+        object ShowGridData();
+        [OperationContract]
+        object ShowData();
+        [OperationContract]
+        string SaveFile();
+        [OperationContract]
+        void EditData(string pathForEdit, string editEvent, string editPerson, string editPeisaj, string editLoc, string editAltele);
+        [OperationContract]
+        string FindData(string result, string editEvent, string editPerson, string editPeisaj, string editLoc, string editAltele);
+    }
+}
---
+++ proiectul 3/ObjectWCF/ModelAndApi.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using ModelAndApi;
+
+namespace ObjectWCF
+{
+    public class ModelAndApi : IMedia
+    {
+        void IMedia.SaveMedia(string path, string events, string persons, string peisaj, string locatie, string altele, DateTime creationDate)
+        {
+            API api = new API();
+            api.SaveMedia(path, events, persons, peisaj, locatie, altele, creationDate);
+        }
+
+        void IMedia.DeleteMedia(string path)
+        {
+            API api = new API();
+            api.DeleteMedia(path);
+        }
+
+        object IMedia.ShowGridData()
+        {
+            API api = new API();
+            return api.ShowGridData();
+        }
+
+        object IMedia.ShowData()
+        {
+            API api = new API();
+            return api.ShowData();
+        }
+
+        string IMedia.SaveFile()
+        {
+            API api = new API();
+            return api.SaveFile();
+        }
+
+        void IMedia.EditData(string pathForEdit, string editEvent, string editPerson, string editPeisaj, string editLoc, string editAltele)
+        {
+            API api = new API();
+            api.EditData(pathForEdit, editEvent, editPerson, editPeisaj, editLoc, editAltele);
+        }
+
+        string IMedia.FindData(string result, string editEvent, string editPerson, string editPeisaj, string editLoc, string editAltele)
+        {
+            API api = new API();
+            return api.FindData(result, editEvent, editPerson, editPeisaj, editLoc, editAltele);
+        }
+    }
+}
